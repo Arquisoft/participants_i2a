b585ee09c4150ec8e97d3e4cfd097bb1fc3b61a6
alexbrks <alex_brks@hotmail.com>
2017-02-20 18:09:09 +0100

    test

---
+++ src/main/java/hello/Adapter.java
@@ -0,0 +1,9 @@
+package hello;
+
+/**
+ * Created by yy on 20-Feb-17.
+ */
+public class Adapter {
+
+
+}
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,33 @@
 package hello;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Date;
 
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(ParticipantRepository repository){
+
+        return (args) -> {
+            repository.save(new Participant("pepe", "pepe", "pepe@pepe.com", new Date(123),
+                    "pepe", "pepe", "pepe", "pepe"));
+            Participant participant = repository.findParticipantByEmailAndPassword
+                    ("pepe@pepe.com", "pepe");
+            log.info(participant.toString());
+        };
     }
 }
